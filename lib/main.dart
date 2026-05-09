324b1905a0d86bea7742abe1f973225544e33ae3
saleem faeq <saleemfaeq@gmail.com>
2026-05-10 01:03:10 +0300

    Update welcome text in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,7 @@ class SimpleProject extends StatelessWidget {
       // The body of the Scaffold is a Center widget that contains a Text widget
       body: Center(
         child: Text(
-          'Welcome to Facebook 👋',
+          'Welcome to Facebook XX 👋',
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold,
           color:Color.fromARGB(255, 190, 69, 206)),
         ),
